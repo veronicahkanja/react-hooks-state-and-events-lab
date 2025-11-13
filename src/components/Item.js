111eda84557d26aded0b9fe5136c5849c9b4924d
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-11-13 15:22:12 +0300

    ninth lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+  function handleAddToCartClick() {
+    setIsInCart(!isInCart);
+  }
+  const liClass = isInCart ? "in-cart" : "";
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+  const buttonClass = isInCart ? "remove" : "add";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleAddToCartClick}>{buttonText}</button>
     </li>
   );
 }
